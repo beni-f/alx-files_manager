e8220d6c459eca9070ae48f7024df9f932fa904d
beni-f <benifissha@gmail.com>
2024-08-03 18:14:25 +0300

    Redis Utils

---
+++ utils/redis.js
@@ -0,0 +1,60 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.connected = true;
+
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+
+    this.client.on('err', (err) => {
+      console.log(err);
+      this.connected = false;
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
