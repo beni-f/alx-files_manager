bcf401d5aee3a3b53cbf5b5fb03b60566175c9c8
beni-f <benifissha@gmail.com>
2024-08-04 20:19:42 +0300

    updated

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,36 +1,36 @@
 const sha1 = require('sha1');
 const dbClient = require('../utils/db');
 
-export default class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+async function postNew(req, res) {
+  const { email, password } = req.body;
 
-    if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-    }
-
-    if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-    }
-
-    const userExists = await dbClient.client.db().collection('users').findOne({ email });
-
-    if (userExists) {
-      return res.status(400).json({ error: 'Already exist' });
-    }
-
-    const hashedPassword = sha1(password);
-
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
-
-    const result = await dbClient.db.collection('users').insertOne(newUser);
-
-    return res.status(201).json({
-      id: result.insertedId.toString(),
-      email,
-    });
+  if (!email) {
+    res.status(400).json({ error: 'Missing email' });
   }
+
+  if (!password) {
+    res.status(400).json({ error: 'Missing password' });
+  }
+
+  const userExists = await dbClient.client.db().collection('users').findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ error: 'Already exist' });
+  }
+
+  const hashedPassword = sha1(password);
+
+  const newUser = {
+    email,
+    password: hashedPassword,
+  };
+
+  const result = await dbClient.db.collection('users').insertOne(newUser);
+
+  return res.status(201).json({
+    email,
+    id: result.insertedId.toString(),
+  });
 }
+
+module.exports = { postNew };
