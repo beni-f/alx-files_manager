b43156ce1ded6bc2e4f2cc9ede64dc2a648df44b
beni-f <benifissha@gmail.com>
2024-08-05 18:50:50 +0300

    User Authorization

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { getStats, getStatus } = require('../controllers/AppController');
 const { postNew } = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController')
 
 const router = express.Router();
 
 router.get('/status', getStatus);
 router.get('/stats', getStats);
 router.post('/users', postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
 
 module.exports = router;
